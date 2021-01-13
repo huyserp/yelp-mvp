77bba387a2f8ed485b314f5d9c706644419289d1
peter huyser <huyserp@gmail.com>
2021-01-13 19:14:04 +0100

    restaurant and review controllers complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-
+  NUMBER_SET = [0, 1, 2, 3, 4, 5]
   validates :content, presence: true
   validates :rating, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: NUMBER_SET }, numericality: { only_integer: true }
 end
